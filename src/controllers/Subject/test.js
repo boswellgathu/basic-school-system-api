fa5047b77c0379dc0a26250e9a41bdc3c2be21e6
Boswell Gathu <boswell.gathu@andela.com>
2018-11-29 17:30:13 +0300

    add more subject controller tests

--- src/controllers/Subject/test.js
+++ src/controllers/Subject/test.js
@@ -7,7 +7,7 @@ const { LIVE, VALIDATION, ARCHIVED } = require('../../../db/constants');
 const app = require('../../../index');
 
 describe('Subject Controller', () => {
-  describe('Post /api', () => {
+  describe('POST /api', () => {
     let subject = {
       name: 'Controlling Drones for dummies'
     };
@@ -60,6 +60,67 @@ describe('Subject Controller', () => {
           expect(res.body.name).toBe(subject.name);
           expect(res.body.status).toBe(LIVE);
           expect(res.body.teacherId).toBe(teacher.id);
+          done();
+        });
+    });
+    it('/subject', (done) => {
+      subject = { name: 'herding ants for capibaras' };
+      request(app)
+        .post('/api/subject')
+        .send(subject)
+        .set('Accept', 'application/json')
+        .set('x-access-token', token)
+        .expect(400)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.body.Error).toBe('SequelizeUniqueConstraintError: Validation error');
+          done();
+        });
+    });
+  });
+
+  describe('PUT /api/subject:id', () => {
+    let createdSubject;
+    let token;
+    before(async () => {
+      const subject = {
+        name: 'Controlling Drones for dummies'
+      };
+      createdSubject = await Subject.create(subject, { raw: true });
+      const adminUser = await fetchAdmin();
+      token = generateToken({ id: adminUser.id });
+    });
+
+    after(() => {
+      Subject.destroy({ truncate: true, cascade: true });
+    });
+
+    it('updates subject name', (done) => {
+      request(app)
+        .put(`/api/subject/${createdSubject.id}`)
+        .send({ name: 'what a lame name' })
+        .set('Accept', 'application/json')
+        .set('x-access-token', token)
+        .expect(200)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.body.name).toBe('what a lame name');
+          expect(res.body.status).toBe(VALIDATION);
+          expect(res.body.teacherId).toBe(null);
+          done();
+        });
+    });
+
+    it('returns 404 when subject is not found', (done) => {
+      request(app)
+        .put('/api/subject/2344')
+        .send({ name: 'what a lame name' })
+        .set('Accept', 'application/json')
+        .set('x-access-token', token)
+        .expect(404)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.body.Error).toBe('Subject: 2344 does not exist');
           done();
         });
     });
